3f1181157abcce37a727d4d33992565c5fff3527
Geoff <geoff.oxholm@gmail.com>
2013-08-22 13:19:13 -0400

    first full version

--- uploadr.py
+++ uploadr.py
@@ -4,24 +4,33 @@ import sys
 import operator
 from glob import glob
 from itertools import groupby
+import argparse
 
 KEY    = "63fc8ff7f59e7a40d344be4574471d0c"
 SECRET = "d58873849eec23fa"
-ROOT   = "/data/Pictures/"
+ROOT   = "/data/Pictures/" # Default root to use
+TOKEN = ".uploadr_token"
+
+# Permissions (0 or 1)
+PUBLIC = 0
+FRIEND = 1
+FAMILY = 1
 
 class Uploadr(object):
-    def __init__(self, key, secret):
+    def __init__(self, key, secret, root):
         flickr.set_keys(key, secret)
-        self.authorize()
+        self.authorize(root)
         self.user = flickr.test.login()
-        self.photos = []
-        self.photoset = None
 
-    def authorize(self):
-        token_path = os.path.join(ROOT, TOKEN)
+    def authorize(self, path):
+        root = path
+        while not os.path.exists(os.path.join(root, TOKEN)) and root is not "/":
+            root = os.path.dirname(root)
+        token_path = os.path.join(root, TOKEN)
         if os.path.exists(token_path):
             flickr.set_auth_handler(token_path)
         else:
+            token_path = os.path.join(path, TOKEN)
             a = flickr.auth.AuthHandler()
             print "Please go to the following URL, and enter the oauth_verifier:"
             print "                                              --------------"
@@ -46,39 +55,90 @@ class Uploadr(object):
                     photos = photoset.getPhotos()
                     for page in range(photos.info.pages - 1):
                         photos.extend(photoset.getPhotos(page = page + 2))
-                    self.photos = list(photos)
-                    self.set = photoset
-                    self.photos.sort(key = operator.attrgetter("title"))
-                    print "Found %d previously uploaded photos" % len(self.uploading)
-                    return
-        print "Did not find any previously uploaded photos"
-        self.photos = []
-        self.photoset = None
+                    photographs = list(photos)
+                    photographs.sort(key = operator.attrgetter("title"))
+                    return photoset, photographs
+        return None, []
 
-    def get_duplicates(self):
-        duplicates = []
-        for k, g in groupby(self.uploading, key = operator.attrgetter('title')):
-            group = list(g)
-            for duplicate in group[1:]:
-                duplicates.append(duplicate)
+    def upload(self, path, photoset):
+        picture = flickr.upload(photo_file = path, title = os.path.splitext(os.path.basename(path))[0], is_public = PUBLIC, is_friend = FRIEND, is_family = FAMILY)
+        if photoset is None:
+            photoset = flickr.Photoset.create(title = os.path.basename(os.path.dirname(path)), primary_photo = picture)
+        photoset.addPhoto(photo = picture)
+        return picture, photoset
 
-    def delete_duplicates(self, duplicates = None):
-        if duplicates is None:
-            duplicates = self.get_duplicates()
-        for duplicate in duplicates:
-            print "Deleting %s" % duplicate.id
-            duplicate.delete()
+
+def get_duplicates(photos):
+    duplicates = []
+    for k, g in groupby(photos, key = operator.attrgetter('title')):
+        group = list(g)
+        for duplicate in group[1:]:
+            duplicates.append(duplicate)
+    return duplicates
+
+def delete_duplicates(duplicates):
+    for duplicate in duplicates:
+        print "Deleting %s" % duplicate.id
+        duplicate.delete()
+
+def get_photos(path):
+    photos = []
+    for extensions in ['jpg', 'JPG', 'jpeg', 'JPEG']:
+        photos.extend( glob(os.path.join(path, "*.%s" % extensions)) )
+    items = map(os.path.basename, photos)
+    items.sort()
+    return items
+
+def sync(path, uploadr):
+    print path
+    print "=" * len(path)
+    photoset, remote = uploadr.load(os.path.basename(path))
+    local = get_photos(path)
+
+    print "Found %d local photos" % len(local)
+    print "Found %d already uploaded photos" % len(remote)
+
+    duplicates = get_duplicates(remote)
+    if len(duplicates) > 0:
+        print "Found %d duplicates" % len(duplicates)
+        delete_duplicates(duplicates)
+
+
+    remote_names = [item.title for item in remote]
+
+    skipped = 0
+    for item in local:
+        title = os.path.splitext(item)[0]
+        picture_path = os.path.join(path, item)
+        if title in remote_names:
+            skipped += 1
+            continue
+        if skipped > 0:
+            print "Skipped %d already uploaded images" % skipped
+            skipped = 0
+        print "Uploading", picture_path
+        picture, photoset = uploadr.upload(picture_path, photoset)
+
+    print "Done!"
 
 
 if __name__ == "__main__":
-    uploadr = Uploadr(KEY, SECRET)
-    for path in glob(os.path.join(ROOT, "*")):
-        if path is os.path.dir(path):
-            if os.path.exists(os.path.join(path, ".private")):
-                continue
-            print path
-            #uploadr.load(os.path.basename(path)
+    parser = argparse.ArgumentParser(description = "Syncs directories with Flickr")
+    parser.add_argument("path", nargs="*", help = "Specific directories to sync",
+                        default = [path for path in glob(os.path.join(ROOT, "*")) \
+                                       if os.path.isdir(path) and not os.path.exists(os.path.join(path, ".private"))])
+    args = parser.parse_args()
+    args.path = [path.rstrip('/') for path in args.path]
 
+    for path in args.path:
+        if not os.path.exists(path):
+            print "Error! `%s` does not exist" % path
+            sys.exit(0)
+
+    uploadr = Uploadr(KEY, SECRET, args.path[0])
+    for path in args.path:
+        sync(path, uploadr)
+        print ""
 
 
 
