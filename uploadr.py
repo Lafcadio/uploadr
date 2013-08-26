2119ae0361fd6b23066ce16748a1d3eb1208b030
Geoff O <gao25@drexel.edu>
2013-08-26 17:17:48 -0400

    bug fix, and added support for pushing and pulling, as well as deleting missing files

--- uploadr.py
+++ uploadr.py
@@ -64,7 +64,8 @@ class Uploadr(object):
         picture = flickr.upload(photo_file = path, title = os.path.splitext(os.path.basename(path))[0], is_public = PUBLIC, is_friend = FRIEND, is_family = FAMILY)
         if photoset is None:
             photoset = flickr.Photoset.create(title = os.path.basename(os.path.dirname(path)), primary_photo = picture)
-        photoset.addPhoto(photo = picture)
+        else:
+            photoset.addPhoto(photo = picture)
         return picture, photoset
 
 
@@ -89,7 +90,7 @@ def get_photos(path):
     items.sort()
     return items
 
-def sync(path, uploadr):
+def sync(path, uploadr, action, delete, really = False):
     print path
     print "=" * len(path)
     photoset, remote = uploadr.load(os.path.basename(path))
@@ -105,19 +106,52 @@ def sync(path, uploadr):
 
 
     remote_names = [item.title for item in remote]
+    local_names  = [os.path.splitext(item)[0] for item in local]
 
-    skipped = 0
-    for item in local:
-        title = os.path.splitext(item)[0]
-        picture_path = os.path.join(path, item)
-        if title in remote_names:
-            skipped += 1
-            continue
+
+    if action in ["sync", "push"] or delete:
+        skipped = 0
+        for item, title in zip(local, local_names):
+            picture_path = os.path.join(path, item)
+            if title in remote_names:
+                skipped += 1
+                continue
+            if skipped > 0:
+                print "Skipped %d remote images already %sed" % (skipped, action)
+                skipped = 0
+
+            if action in ["sync", "push"]:
+                print "Uploading", picture_path
+                if really:
+                    picture, photoset = uploadr.upload(picture_path, photoset)
+            elif delete:
+                print "Deleting", picture_path
+                if really:
+                    os.remove(picture_path)
         if skipped > 0:
-            print "Skipped %d already uploaded images" % skipped
-            skipped = 0
-        print "Uploading", picture_path
-        picture, photoset = uploadr.upload(picture_path, photoset)
+            print "Skipped %d local images already %sed" % (skipped, action)
+
+    if action in ["sync", "pull"] or delete:
+        skipped = 0
+        for item in remote:
+            if item.title in local_names:
+                skipped += 1
+                continue
+            if skipped > 0:
+                print "Skipped %d remote images already %sed" % (skipped, action)
+                skipped = 0
+
+            if action in ["sync", "pull"]:
+                local_path = os.path.join(path, "%s.%s" % (item.title, "jpg"))
+                print "Downloading", item.title, "as", local_path
+                if really:
+                    item.save(local_path, "Original")
+            elif delete:
+                print "Deleting", item.title
+                if really:
+                    item.delete()
+        if skipped > 0:
+            print "Skipped %d remote images already %sed" % (skipped, action)
 
     print "Done!"
 
@@ -127,8 +161,16 @@ if __name__ == "__main__":
     parser.add_argument("path", nargs="*", help = "Specific directories to sync",
                         default = [path for path in glob(os.path.join(ROOT, "*")) \
                                        if os.path.isdir(path) and not os.path.exists(os.path.join(path, ".private"))])
+    parser.add_argument("--action", dest="action", nargs=1, choices=["push", "pull", "sync"], default="push", help = "Push = upload (default), pull = download, sync = both")
+    parser.add_argument("--delete-missing", dest="delete", action="store_true", default=False, help = "Whether or not to delete when a file at the destination is not at the source")
+    parser.add_argument("--preview", dest="really", action="store_false", default=True, help = "Only pretend to do actions")
     args = parser.parse_args()
     args.path = [path.rstrip('/') for path in args.path]
+
+    action = args.action[0]
+    if action == "sync" and args.delete:
+        print "The --delete-missing option makes no sense for the `sync` action"
+        sys.exit(0)
 
     for path in args.path:
         if not os.path.exists(path):
@@ -137,7 +179,7 @@ if __name__ == "__main__":
 
     uploadr = Uploadr(KEY, SECRET, args.path[0])
     for path in args.path:
-        sync(path, uploadr)
+        sync(path, uploadr, action, args.delete, args.really)
         print ""
 
 
